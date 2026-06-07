b29b3a4f6f93a9fe48fd774b6eaebdc9cae29fa8
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2026-06-07 13:32:17 +0300

    feat: add models configurations and app Db context

--- WebApi/Api/Program.cs
+++ WebApi/Api/Program.cs
@@ -1,3 +1,5 @@
+using Infrastructure.Foundation.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api
 {
@@ -7,15 +9,16 @@ namespace Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<HotelManagementDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
---
+++ WebApi/Infrastructure/Foundation/Configurations/ReservationConfiguration.cs
@@ -0,0 +1,48 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Foundation.Configurations;
+
+internal class ReservationConfiguration : IEntityTypeConfiguration<Reservation>
+{
+    public void Configure( EntityTypeBuilder<Reservation> builder )
+    {
+        builder.ToTable( nameof( Reservation ) );
+        builder.HasKey( r => r.Id );
+
+        builder.Property( r => r.GuestName )
+            .HasMaxLength( 200 )
+            .IsRequired();
+
+        builder.Property( r => r.GuestPhoneNumber )
+            .HasMaxLength( 20 );
+
+        builder.Property( r => r.Total )
+            .HasColumnType( "decimal(18,2)" )
+            .IsRequired();
+
+        builder.Property( r => r.Currency )
+            .HasMaxLength( 3 )
+            .IsRequired();
+
+        builder.Property( r => r.ArrivalDate )
+            .IsRequired();
+
+        builder.Property( r => r.DepartureDate )
+            .IsRequired();
+
+        builder.Property( r => r.IsCanceled )
+            .HasDefaultValue( false );
+
+        builder.HasOne<Property>()
+            .WithMany()
+            .HasForeignKey( r => r.PropertyId )
+            .OnDelete( DeleteBehavior.NoAction );
+
+        builder.HasOne<RoomType>()
+            .WithMany()
+            .HasForeignKey( r => r.RoomTypeId )
+            .OnDelete( DeleteBehavior.NoAction );
+    }
+}
